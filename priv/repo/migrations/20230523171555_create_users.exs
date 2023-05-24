2cc2b5ae86824ccf12cbd472efe6da3315983fd3
GabrielMalakias <gabriel07malakias@gmail.com>
2023-05-24 19:13:38 +0200

    Change back to id and create db constraints

--- priv/repo/migrations/20230523171555_create_users.exs
+++ priv/repo/migrations/20230523171555_create_users.exs
@@ -2,11 +2,14 @@ defmodule Punkte.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users, primary_key: false) do
-      add :id, :binary_id, primary_key: true
+    create table(:users) do
       add :points, :integer
 
       timestamps()
     end
+
+    create constraint(:users, :points_must_be_in_range, check: "points >= 0 AND points <= 100")
+
+    execute("ALTER TABLE users SET (fillfactor = 70)")
   end
 end
